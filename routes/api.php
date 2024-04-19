a0a11a37e50225bc7972cf0db56775c4cfeb4a27
dromich <hrushch.rr57@gmail.com>
2024-04-19 17:15:00 +0300

    - api route small fix

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('books', 'App\Http\Controllers\Api\BookController');
+Route::apiResource('books', BookController::class);
